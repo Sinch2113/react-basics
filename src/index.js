248eca0ce5adc6e045d29d5c4532bab710ba3ab6
Sinchana Bharadwaj <70107822+Sinch2113@users.noreply.github.com>
2021-07-22 18:44:24 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Heading</h1>
+    <ul>
+      <li>Physics</li>
+      <li>Chemistry</li>
+      <li>Mathematics</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
